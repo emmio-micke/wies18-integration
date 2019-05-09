7f427c328310a06e1ed10053327a01d2a0f516ce
Mikael Olsson <mikael.olsson@emmio.se>
2019-05-09 09:08:02 +0200

    Lecture 6 about curl with examples.

---
+++ www/exercises/06-curl/03-put.php
@@ -0,0 +1,32 @@
+<pre>
+<?php
+
+// Set URL.
+$url = 'http://jsonplaceholder.typicode.com/posts/3';
+
+$data = '{"title":"Hello","body":"World"}';
+
+// Create a curl instance.
+$ch = curl_init($url);
+
+// Setup curl options
+// To set PUT we need to specify a custom request.
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+
+// This is the data we want to post.
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+// We want curl to return whatever response we get from the server.
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+// We want to specify that we're sending JSON.
+curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+
+// Enable headers.
+curl_setopt($ch, CURLOPT_HEADER, 1);
+
+// Perform the request and get the response.
+$response = curl_exec($ch);
+curl_close($ch);
+
+print_r($response);
